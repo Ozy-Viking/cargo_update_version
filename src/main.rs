501b4f25105d9a31ec469104656a6e4a16e04903
Ozy-Viking <admin@hankin.io>
2025-07-01 17:12:57 +1000

    uncommented

--- src/main.rs
+++ src/main.rs
@@ -28,27 +28,28 @@ fn main() -> miette::Result<()> {
         Level::WARN,
         None,
     )?;
-    // let cargo_manifest = manifest::find_matifest_path(args.manifest_path())?;
-    // let old_version = cargo_manifest
-    //     .get_root_package()
-    //     .expect("Currently under this belief")
-    //     .version();
-
-    // use cli::BumpVersion as BV;
-    // let packages = find_matifest_path(args.manifest_path())?;
-    // let mut cargo_file = manifest::CargoFile::new(packages.cargo_file_path())?;
-    // assert_eq!(&cargo_file.get_root_package_version().unwrap(), old_version);
-    // let new_packages = match args.bump_version() {
-    //     BV::Patch | BV::Minor | BV::Major => {
-    //         manifest::bump_version(args.bump_version(), cargo_manifest)?
-    //     }
-    //     BV::Set(version) => set_version(cargo_manifest, version)?,
-    // };
-
-    // cargo_file.set_root_package_version(new_packages.get_root_package().unwrap().version())?;
-    // cargo_file.write_cargo_file()?;
 
     Git::is_dirty()?;
+    let cargo_manifest = manifest::find_matifest_path(args.manifest_path())?;
+    let old_version = cargo_manifest
+        .get_root_package()
+        .expect("Currently under this belief")
+        .version();
+
+    use cli::BumpVersion as BV;
+    let packages = find_matifest_path(args.manifest_path())?;
+    let mut cargo_file = manifest::CargoFile::new(packages.cargo_file_path())?;
+    assert_eq!(&cargo_file.get_root_package_version().unwrap(), old_version);
+    let new_packages = match args.bump_version() {
+        BV::Patch | BV::Minor | BV::Major => {
+            manifest::bump_version(args.bump_version(), cargo_manifest)?
+        }
+        BV::Set(version) => set_version(cargo_manifest, version)?,
+    };
+
+    cargo_file.set_root_package_version(new_packages.get_root_package().unwrap().version())?;
+    cargo_file.write_cargo_file()?;
+
     Ok(())
 }
 
