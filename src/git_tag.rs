8cc99d172784e6fe06088654e4095962b275feaf
Ozy-Viking <admin@hankin.io>
2025-07-01 16:44:35 +1000

    clean slate

--- src/git_tag.rs
+++ src/git_tag.rs
@@ -4,3 +4,23 @@
 //! 2. Add change/hunk.
 //! 3. Commit just the hunk with version change.
 //! 4. Tag the commit.
+
+use std::process::Command;
+
+use miette::IntoDiagnostic;
+use tracing::instrument;
+
+pub struct Git;
+
+impl Git {
+    #[instrument]
+    pub fn is_dirty() -> miette::Result<bool> {
+        let mut git_status = Command::new("git");
+        git_status.args(["status", "--short"]);
+        let out = git_status.output().into_diagnostic()?;
+
+        let out_string = String::from_iter(out.stdout.iter().map(|&i| char::from(i)));
+        dbg!(out_string.lines().count());
+        Ok(true)
+    }
+}
